9e40126299fa6dcecb51386dfeaee1d604a7d230
kazu24z <progra.taro2021@gmail.com>
2023-11-19 20:35:16 +0900

    パスワードの暗号化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_17_142834) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_19_113214) do
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
@@ -22,10 +22,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_17_142834) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.string "jti"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "posts", "users"
